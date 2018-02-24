70745bb209c59f33e56475899eb39610327723f1
Bhasheyam <bhasheyam09@gmail.com>
2018-02-24 17:25:59 -0600

    UI

---
+++ WebContent/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * # AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o
+ * 53eeb015d61056c54245a909c79862532fc953ad
+ * https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=YOUR_API_KEY
+ * https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o
+ * https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o
+ * CmRaAAAASWR4YMPwR22KxGNUD9Awfa6lhrHnv3wgESL_Q0JsBaYpLy-e7udGVtpV_tctx4Xcr8_Y6qdEitm9lEVq3fS317tltC5p78fZS5r1Vj1YOWeTiGEomW_PiRFps8TKA0ajEhCw5cto-FoLvyIG8SLQQMdEGhSibsoTVybi0B9445EjmewmrJ73fg
+ */
