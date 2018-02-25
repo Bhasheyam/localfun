0f8c27d7c4117266f5d15c28b66bbc5a96418c86
Bhasheyam <bhasheyam09@gmail.com>
2018-02-24 19:28:35 -0600

    CORS

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -6,3 +6,24 @@
  * https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o
  * CmRaAAAASWR4YMPwR22KxGNUD9Awfa6lhrHnv3wgESL_Q0JsBaYpLy-e7udGVtpV_tctx4Xcr8_Y6qdEitm9lEVq3fS317tltC5p78fZS5r1Vj1YOWeTiGEomW_PiRFps8TKA0ajEhCw5cto-FoLvyIG8SLQQMdEGhSibsoTVybi0B9445EjmewmrJ73fg
  */
+
+
+function findlocation(location){
+	var txt =  document.getElementById("input").value;
+	
+	
+	gapi.load('auth2', function() {
+    // Library loaded. 
+		});
+	var restRequest = gapi.client.request({
+		  'path': 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+		  'params': {'query' : txt,  'key' : 'AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o'}
+		});
+	console.log(typeof (restRequest))
+	//var uurl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+txt+"&key=AIzaSyAMYkvdmY0vRhAHhibXpP4_HcJuCLOJp1o";
+	
+	
+	//console.log(dataloc);
+	
+}
+	
